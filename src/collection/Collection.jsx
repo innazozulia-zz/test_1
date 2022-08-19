a3f748460ec5af8eea7866c9d1d699b231385566
inna <innazozulia@outlook.com>
2022-08-19 21:44:12 +0300

    apdate component

--- src/collection/Collection.jsx
+++ src/collection/Collection.jsx
@@ -3,8 +3,10 @@ import CollectionItem from "./CollectionItem";
 import "./indexCollection.scss";
 
 const Collection = () => {
+  //state for render
   const [collection, setCollection] = React.useState([]);
 
+  // first render
   React.useEffect(() => {
     fetch("https://62ffc85b9350a1e548e65559.mockapi.io/collection")
       .then((res) => res.json())
@@ -16,6 +18,7 @@ const Collection = () => {
         alert("Loading Error. Please try again.");
       });
   }, []);
+
   return (
     <div className="wrapper__collection">
       <h1>My photo collection</h1>
@@ -30,15 +33,9 @@ const Collection = () => {
         <input className="search-input" placeholder="Search ..." />
       </div>
       <div className="content">
-        <CollectionItem
-          name="Travel around the world"
-          images={[
-            "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-          ]}
-        />
+        {collection.map((obj) => (
+          <CollectionItem name={obj.name} images={obj.photos} />
+        ))}
       </div>
       <ul className="pagination">
         <li>1</li>
